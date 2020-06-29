1ffa28dc0fc542d9273fe864fed28b277aac7491
Jessie Donovan <donovanajessie@gmail.com>
2020-06-29 02:09:53 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_values method
- 
+
 def get_the_values(groceries)
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
